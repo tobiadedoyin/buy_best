03cc4fdd511110770c40941fd32dfd74eec6b562
Tobi Oladele <oladeletobiadedoyin@gmail.com>
2024-11-15 04:58:15 +0100

    project folders structured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'src/config/data-source';
 
 @Module({
   imports: [TypeOrmModule.forRoot(AppDataSource)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
